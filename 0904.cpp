40e12e379b02df1f0fdb78e74794d605d3863368
hxxn05 <160487499+hxxn05@users.noreply.github.com>
2024-09-04 16:03:42 +0900

    Add files via upload

---
+++ 0904.cpp
@@ -0,0 +1,29 @@
+
+// # 포인터의 기본 형식
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int num;
+//	int* pnum;
+//	pnum = &num;
+//	*pnum = 10;
+//
+//	printf("변수명으로 num 값 출력 : %d\n", num);
+//	printf("포인터으로 num 값 출력 : %d\n", *pnum);
+//
+//	return 0;
+//}
+
+
+//# 메모리상 주소 구하는 법
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int num = 10;
+//
+//	printf("%p\n", &num);
+//
+//	return 0;
+//}
